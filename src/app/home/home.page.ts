bf05209a9387b6f8b13edf58d40a397e671dba35
Derian-18 <derian.jimenezramirez@cesunbc.edu.mx>
2026-05-25 10:47:59 -0700

    feat: Se creo el proyecto y la interfaz, y dos tareas en un array

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, 
+  IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/angular/standalone';
+import { IonButton } from '@ionic/angular/standalone';
+import { Task } from '../model/task.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, 
+    IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonButton],
 })
 export class HomePage {
-  constructor() {}
+
+  // Arreglo de tareas
+  task: Task[] = [{
+    id: 1,
+    titulo: 'Configuracion de Ionic',
+    descripcion: 'Instalar Node, Angular y tambien Ionic',
+    finalizado: false,
+    prioridad: 'alta'
+  },
+
+  {
+    id: 2,
+    titulo: 'Aprender Angular',
+    descripcion: 'Aprender los conceptos basicos de Angular',
+    finalizado: false,
+    prioridad: 'media'
+  },];
+
+  constructor() {
+    console.log('Lista de tareas:');
+    console.log(this.task);
+  }
+
+  saludar() {
+    console.log('Hola mundo, como estas?');
+  }
 }
